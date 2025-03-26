3e8149de12a3999890ce0e30c53e1d5f76c5ebf0
Sulav Gautam <gautamsulav000@gmail.com>
2025-03-26 22:20:07 +0545

    inital commit

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/sulavgautam/Documents/virza/src/app/layout\":[\"static/media/b513684977831128-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
